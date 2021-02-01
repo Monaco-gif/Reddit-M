ed1121aa33eae3a70e7bb7de0e3cc52e7b1d6a25
Monaco-gif <monaco.matteo@iisgalvanimi.edu.it>
2021-02-01 09:53:10 +0000

    punto

---
+++ angular-reddit/src/app/article/article.component.ts
@@ -0,0 +1,23 @@
+import { HostBinding, OnInit } from '@angular/core';
+import { Article } from './article.model';
+
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'card';
+  article:Article
+
+  constructor() {
+    this.article = new Article('Angular 2','http://angular.io',10);
+  }
+
+  voteUp(): Boolean {
+    this.article.voteUp(); //Modificato qui
+    return false;
+  }
+
+  voteDown():Boolean {
+    this.article.voteDown(); //Modificato qui
+    return false;
+  }
+
+  ngOnInit() {}
+}
